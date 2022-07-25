d03a5f6e5af82dd8890adfbc2eee4f5be1783c04
Celinaaaaaa <cebalz@hotmail.com>
2022-07-25 16:37:45 -0300

    Modulo 3 criando section contato, topicos, carrosel e banner

---
+++ assets/js/swipper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 20,
+    slidesPerView: 2.6,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+  });
